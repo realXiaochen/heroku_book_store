616d3a396172d35344fac38e5c6144495e14850b
Z <z@Zs-MacBook-Air.local>
2017-08-14 13:12:52 -0700

    redis

--- src/main/java/com/example/config/HttpSessionConfig.java
+++ src/main/java/com/example/config/HttpSessionConfig.java
@@ -9,7 +9,10 @@ public class HttpSessionConfig {
 
 	@Bean
 	public LettuceConnectionFactory connectionFactory(){ 
-		return new LettuceConnectionFactory();
+		LettuceConnectionFactory l =  new LettuceConnectionFactory();
+		l.setHostName("ec2-34-231-155-48.compute-1.amazonaws.com");
+		l.setPort(32839);
+		l.setPassword("p0c3d8918f9f7202efc237d62c0558e050a64d8d33aa03aed4d9b42d27489d3e8");
+		return l;
 	}	
-		
 }
